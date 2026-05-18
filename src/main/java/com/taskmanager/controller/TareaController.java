586475719fc6f1857a2ac62cfc03b13725e5049e
Kaaapo <u20212200558@usco.edu.co>
2026-05-17 20:33:50 -0500

    chore: Remove duplicate import of HttpStatus in TareaController

--- src/main/java/com/taskmanager/controller/TareaController.java
+++ src/main/java/com/taskmanager/controller/TareaController.java
@@ -6,7 +6,6 @@ import com.taskmanager.service.MembershipPermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
